5e25fe35add3b1789df7182356916b4705c69f2a
Saksham Bangale <sakshambangale779@gmail.com>
2026-07-02 22:37:15 +0530

    05_constants.c
    This repository contains my solutions to various programming and development questions, organized into a clean and structured folder hierarchy. I have updated the repository by adding all solution files, improving the organization, and ensuring each question is stored in its appropriate directory for easier navigation and maintenance.

---
+++ basics/05_constants.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define COUNTRY "India"
+
+int main()
+{
+    const float PI = 3.14159f;
+
+    printf("PI = %.5f\n", PI);
+    printf("Country = %s\n", COUNTRY);
+
+    return 0;
+}
