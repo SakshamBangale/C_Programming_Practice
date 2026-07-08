ffabe37172e1a689814534819b3d664d867dbd68
Saksham Bangale <sakshambangale779@gmail.com>
2026-07-08 23:14:23 +0530

    Add vowel counting program in 08_vowels.c
    This program counts the number of vowels in a user-input string.

---
+++ strings/08_vowels.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int i = 0;
+    int count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0')
+    {
+        char ch = str[i];
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            count++;
+
+        i++;
+    }
+
+    printf("Vowels = %d\n", count);
+
+    return 0;
+}
