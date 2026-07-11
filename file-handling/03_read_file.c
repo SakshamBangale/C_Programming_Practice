8efeb52584c60a5a2185c2f86e63a93803347d0b
Saksham Bangale <sakshambangale779@gmail.com>
2026-07-11 23:34:14 +0530

    Add file reading functionality in 03_read_file.c

---
+++ file-handling/03_read_file.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    FILE *file;
+    char ch;
+
+    file = fopen("sample.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("File not found.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF)
+        putchar(ch);
+
+    fclose(file);
+
+    return 0;
+}
