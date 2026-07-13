8387129d52fc45c0f8a4fec65eaa4329885fb388
Saksham Bangale <sakshambangale779@gmail.com>
2026-07-13 23:04:08 +0530

    Add stack implementation with push and pop functions
    Implement basic stack operations including push, pop, and display.

---
+++ data-structures/01_stack.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int stack[SIZE];
+int top = -1;
+
+void push(int value)
+{
+    if (top == SIZE - 1)
+    {
+        printf("Stack Overflow\n");
+        return;
+    }
+
+    stack[++top] = value;
+}
+
+int pop()
+{
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
+        return -1;
+    }
+
+    return stack[top--];
+}
+
+void display()
+{
+    int i;
+
+    for (i = top; i >= 0; i--)
+        printf("%d ", stack[i]);
+
+    printf("\n");
+}
+
+int main()
+{
+    push(10);
+    push(20);
+    push(30);
+
+    display();
+
+    printf("Popped: %d\n", pop());
+
+    display();
+
+    return 0;
+}
